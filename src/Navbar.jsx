7222762096de2205396e285ded076808b600f8c6
Melody Enyeazu <enyeazumelody205@gmail.com>
2024-09-23 10:36:43 +0100

    sorted dark mode

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -20,6 +20,10 @@ const Navbar = () => {
         refnav.current.style.backgroundColor = "#121212";
         refbtn.current.style.backgroundColor = "#41a719";
         refbtn.current.style.color = "#fff";
+
+        const landing = document.getElementById("land");
+        
+        landing.style.backgroundColor = "#121212";
     }
 
     return(
